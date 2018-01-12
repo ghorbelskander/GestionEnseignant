6e2df858b3727e5344a9384f26204509c165b726
SkanderGhorbel <ghorbel.skander@gmail.com>
2018-01-12 19:51:15 +0100

    initial commit

---
+++ src/main/java/tn/iit/demo/GestionEnseignantsApplication.java
@@ -0,0 +1,12 @@
+package tn.iit.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionEnseignantsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionEnseignantsApplication.class, args);
+	}
+}
